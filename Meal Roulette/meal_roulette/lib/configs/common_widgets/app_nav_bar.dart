dd772d396010cd83f8089ffaf88f2a261f2e3906
aitsam <aitsamsofficial@gmail.com>
2025-11-09 13:49:54 -0800

    App Nav Bar functionality added in the design

---
+++ Meal Roulette/meal_roulette/lib/configs/common_widgets/app_nav_bar.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:meal_roulette/configs/resources/resources.dart';
+import 'package:meal_roulette/routes/app_routes_constants.dart';
+
+class AppNavBar extends StatefulWidget {
+  final Widget child;
+
+  const AppNavBar({super.key, required this.child});
+
+  @override
+  State<AppNavBar> createState() => _AppNavBarState();
+}
+
+class _AppNavBarState extends State<AppNavBar> {
+  int currentIndex = 0;
+  int previousIndex = 0;
+
+  final tabs = [_NavItem(icon: Icons.home_rounded, label: 'Home', route: AppRouteConstants.home), _NavItem(icon: Icons.people_alt_rounded, label: 'Match', route: AppRouteConstants.matches), _NavItem(icon: Icons.person_rounded, label: 'Profile', route: AppRouteConstants.profile)];
+
+  @override
+  Widget build(BuildContext context) {
+    final location = GoRouterState.of(context).uri.toString();
+    currentIndex = _getSelectedIndex(location);
+
+    // Determine the direction for slide animation
+    final isForward = currentIndex >= previousIndex;
+
+    return Scaffold(
+      body: SafeArea(
+        child: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 400),
+          transitionBuilder: (child, animation) {
+            final offsetAnimation = Tween<Offset>(begin: Offset(isForward ? 1.0 : -1.0, 0), end: Offset.zero).animate(CurvedAnimation(parent: animation, curve: Curves.easeInOutCubic));
+            return SlideTransition(position: offsetAnimation, child: child);
+          },
+          child: KeyedSubtree(key: ValueKey(location), child: widget.child),
+        ),
+      ),
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          indicatorColor: R.colors.red.withValues(alpha: 0.1),
+          labelTextStyle: WidgetStateProperty.resolveWith<TextStyle>(
+                (states) {
+              if (states.contains(WidgetState.selected)) {
+                return R.textStyles.font10B.copyWith(color: R.colors.red);
+              }
+              return R.textStyles.font10R.copyWith(color: R.colors.red);
+            },
+          ),
+          iconTheme: WidgetStateProperty.resolveWith<IconThemeData>(
+                (states) => IconThemeData(
+              color: states.contains(WidgetState.selected)
+                  ? R.colors.white
+                  : R.colors.red,
+            ),
+          ),
+        ),
+        child: NavigationBar(
+          height: 70,
+          elevation: 5,
+          indicatorColor: R.colors.red,
+          selectedIndex: currentIndex,
+          onDestinationSelected: (index) {
+            context.go(tabs[index].route);
+          },
+          destinations: tabs.map((tab) => NavigationDestination(icon: Icon(tab.icon), label: tab.label)).toList(),
+        ),
+      ),
+    );
+  }
+
+  int _getSelectedIndex(String location) {
+    if (location.startsWith(AppRouteConstants.home)) return 0;
+    if (location.startsWith(AppRouteConstants.matches)) return 1;
+    if (location.startsWith(AppRouteConstants.profile)) return 2;
+    return 0;
+  }
+}
+
+class _NavItem {
+  final IconData icon;
+  final String label;
+  final String route;
+
+  const _NavItem({required this.icon, required this.label, required this.route});
+}
--- Meal Roulette/meal_roulette/lib/routes/app_routes.dart
+++ Meal Roulette/meal_roulette/lib/routes/app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:meal_roulette/configs/common_widgets/app_nav_bar.dart';
 import 'package:meal_roulette/configs/utils/aap_utils.dart';
 import 'package:meal_roulette/modules/auth/presentation/view/auth_screen.dart';
 import 'package:meal_roulette/modules/details/presentation/view/details_view.dart';
@@ -13,6 +14,7 @@ import 'app_routes_constants.dart';
 
 final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> _shellNavigatorKey = GlobalKey<NavigatorState>();
 
 BuildContext getContext() => rootNavigatorKey.currentContext!;
 
@@ -37,23 +39,54 @@ class AppRoutes {
       GoRoute(
         path: AppRouteConstants.auth,
         name: AppRouteConstants.auth,
-          //pageBuilder: (context, state) => Utils().buildPageWithSlideTransition(context: context, state: state, child: AuthView()), routes: [],
-          pageBuilder: (context, state) => Utils().buildPageWithSlideTransition(context: context, state: state, child: AuthView()), routes: [],
+        //pageBuilder: (context, state) => Utils().buildPageWithSlideTransition(context: context, state: state, child: AuthView()), routes: [],
+        pageBuilder: (context, state) => Utils().buildPageWithSlideTransition(context: context, state: state, child: AuthView()),
+        routes: [],
       ),
-      GoRoute(path: AppRouteConstants.home, name: AppRouteConstants.home, pageBuilder: (context, state) => Utils().buildPageWithSlideTransition(context: context, state: state, child: MensaView()), routes: [
-        GoRoute(path: AppRouteConstants.matches, name: AppRouteConstants.matches, pageBuilder: (context, state) => Utils().buildPageWithRightToLeftTransition(context: context, state: state,child: MatchingView()), routes: [
+
+      ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        builder: (context, state, child) {
+          return AppNavBar(child: child);
+        },
+        routes: [
+          // HOME TAB
+          GoRoute(
+            path: AppRouteConstants.home,
+            name: AppRouteConstants.home,
+            pageBuilder: (context, state) => NoTransitionPage(child: MensaView()),
+            routes: [
+              GoRoute(
+                path: AppRouteConstants.details,
+                name: AppRouteConstants.details,
+                pageBuilder: (context, state) => Utils().buildPageWithRightToLeftTransition(
+                  context: context,
+                  state: state,
+                  child: DetailsView(mensa: state.extra as MensaModel),
+                ),
+                routes: [],
+              ),
+              // GoRoute(path: AppRouteConstants.notifications, name: AppRouteConstants.notifications, pageBuilder: (context, state) => NoTransitionPage(child: NotificationsView()), routes: []),
+              // GoRoute(path: AppRouteConstants.settings, name: AppRouteConstants.settings, pageBuilder: (context, state) => NoTransitionPage(child: SettingsView()), routes: []),
+              // GoRoute(path: AppRouteConstants.contact, name: AppRouteConstants.contact, pageBuilder: (context, state) => NoTransitionPage(child: ContactView()), routes: []),
+              // GoRoute(path: AppRouteConstants.privacy, name: AppRouteConstants.privacy, pageBuilder: (context, state) => NoTransitionPage(child: PrivacyView()), routes: []),
+            ],
+          ),
+          GoRoute(
+            path: AppRouteConstants.matches,
+            name: AppRouteConstants.matches,
+            //pageBuilder: (context, state) => Utils().buildPageWithRightToLeftTransition(context: context, state: state, child: MatchingView()),
+            pageBuilder: (context, state) => NoTransitionPage(child: MatchingView()),
+            routes: [],
+          ),
           GoRoute(
             path: AppRouteConstants.profile,
             name: AppRouteConstants.profile,
             pageBuilder: (context, state) => NoTransitionPage(child: ProfileView()),
             routes: [],
           ),
-         ]),
-        GoRoute(path: AppRouteConstants.details, name: AppRouteConstants.details, pageBuilder: (context, state) => Utils().buildPageWithRightToLeftTransition(context: context, state: state,child: DetailsView(mensa: state.extra as MensaModel)), routes: []),
-        // GoRoute(path: AppRouteConstants.notifications, name: AppRouteConstants.notifications, pageBuilder: (context, state) => NoTransitionPage(child: NotificationsView()), routes: []),
-        // GoRoute(path: AppRouteConstants.settings, name: AppRouteConstants.settings, pageBuilder: (context, state) => NoTransitionPage(child: SettingsView()), routes: []),
-        // GoRoute(path: AppRouteConstants.contact, name: AppRouteConstants.contact, pageBuilder: (context, state) => NoTransitionPage(child: ContactView()), routes: []),
-        // GoRoute(path: AppRouteConstants.privacy, name: AppRouteConstants.privacy, pageBuilder: (context, state) => NoTransitionPage(child: PrivacyView()), routes: []),
-      ],),],
+        ],
+      ),
+    ],
   );
 }
